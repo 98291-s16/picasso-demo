8acce78bcb7f4a572994ec7e988a87b052848df6
Rohan Jadvani <rj1030@gmail.com>
2016-03-24 17:06:05 -0400

    Standard loading

--- app/src/main/java/com/main/picassodemo/MainActivity.java
+++ app/src/main/java/com/main/picassodemo/MainActivity.java
@@ -1,14 +1,27 @@
 package com.main.picassodemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String IMG_URL = "http://i.imgur.com/HFEFssS.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadPicassoDemo();
+    }
+
+    private void loadPicassoDemo() {
+        ImageView im = (ImageView) findViewById(R.id.test_image);
+        Picasso.with(this)
+                .load(IMG_URL)
+                .into(im);
     }
 
 }
